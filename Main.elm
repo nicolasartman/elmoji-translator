e47835e74badc7ec8553d956c9b4880964f9dfe0
Nick Artman <nicolasartman@gmail.com>
2018-02-25 13:58:05 -0800

    Move model, update, and view into main.elm

--- Main.elm
+++ Main.elm
@@ -1,15 +1,69 @@
 module Main exposing (..)
 
 import Html
-import View
-import Model
-import Update
+import Html.Attributes
+import Html.Events
 
 
-main : Program Never Model.Model Update.Msg
+main : Program Never Model Msg
 main =
     Html.beginnerProgram
-        { model = Model.init
-        , view = View.view
-        , update = Update.update
+        { model = init
+        , view = view
+        , update = update
         }
+
+
+type alias Model =
+    { currentText : String }
+
+
+init : Model
+init =
+    { currentText = "" }
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.div
+        []
+        [ Html.node "link"
+            [ Html.Attributes.rel "stylesheet"
+            , Html.Attributes.href "stylesheets/main.css"
+            ]
+            []
+        , Html.nav
+            []
+            [ Html.div
+                [ Html.Attributes.class "nav-wrapper light-blue lighten-2" ]
+                [ Html.div
+                    [ Html.Attributes.class "brand-logo center" ]
+                    [ Html.text "Elmoji Translator" ]
+                ]
+            ]
+        , Html.section
+            [ Html.Attributes.class "container" ]
+            [ Html.div
+                [ Html.Attributes.class "input-field" ]
+                [ Html.input
+                    [ Html.Attributes.type_ "text"
+                    , Html.Attributes.class "center"
+                    , Html.Attributes.placeholder "Let's Translate!"
+                    , Html.Events.onInput SetCurrentText
+                    ]
+                    []
+                ]
+            ]
+        ]
+
+
+type Msg
+    = SetCurrentText String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SetCurrentText newText ->
+            -- currently, this does nothing!
+            model
